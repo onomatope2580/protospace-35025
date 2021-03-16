3c2f42a9f106fc659257b746c252ffa8f39eb35d
onomatope2580 <79963946+onomatope2580@users.noreply.github.com>
2021-03-16 19:19:45 +0900

    second change

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype_id)
     else
       @prototype = Prototype.find(params[:prototype_id])
+      @comment = Comment.new
       @comments = @prototype.comments.includes(:user)
       render "prototypes/show"
     end
